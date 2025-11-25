62c2423ecfc2439f568b30a89b79da61f145fd33
framor81 <francisco.moralespuente2022@gmail.com>
2025-11-25 04:10:26 -0800

    Alt text

--- src/app/components/imageGallery.tsx
+++ src/app/components/imageGallery.tsx
@@ -15,8 +15,8 @@ const imageSets = [
   ],
   [
     { src: "/about/Ara.jpeg", alt: "Image 7" },
-    { src: "/about/RockClimbing.JPG", alt: "Image 8" },
-    { src: "/about/Pickup.jpg", alt: "Image 9" },
+    { src: "/about/RockClimbing.JPG", alt: "Rock Climbing" },
+    { src: "/about/Pickup.jpg", alt: "Pickup" },
     { src: "/about/Sequoia2.JPG", alt: "Image 10" },
     { src: "/about/Sequoia3.JPG", alt: "Image 11" },
     { src: "/about/Sequoia1.JPG", alt: "Image 12" },
